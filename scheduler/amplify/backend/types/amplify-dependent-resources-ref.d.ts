7544b0347103299deab679516704c34e954be756
hkunzler <hkunzler@gmail.com>
2024-02-08 06:36:17 +0000

    setup

---
+++ scheduler/amplify/backend/types/amplify-dependent-resources-ref.d.ts
@@ -0,0 +1,27 @@
+export type AmplifyDependentResourcesAttributes = {
+  "analytics": {
+    "scheduler": {
+      "Id": "string",
+      "Region": "string",
+      "appName": "string"
+    }
+  },
+  "api": {
+    "scheduler": {
+      "GraphQLAPIEndpointOutput": "string",
+      "GraphQLAPIIdOutput": "string",
+      "GraphQLAPIKeyOutput": "string"
+    }
+  },
+  "auth": {
+    "scheduler6e604758": {
+      "AppClientID": "string",
+      "AppClientIDWeb": "string",
+      "IdentityPoolId": "string",
+      "IdentityPoolName": "string",
+      "UserPoolArn": "string",
+      "UserPoolId": "string",
+      "UserPoolName": "string"
+    }
+  }
+}
